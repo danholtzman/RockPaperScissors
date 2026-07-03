5ac0b70c07217e97c0b65ec6f21dd06a80dc81e5
Dan Holtzman <danholtzman@apple.com>
2026-07-03 11:27:38 -0700

    Refactoring

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -9,36 +9,46 @@ import SwiftUI
 
 enum MoveChoice: CaseIterable {
     case rock, paper, scissors
+    
+    var emoji: String {
+        switch self {
+            case .rock: "🪨"
+            case .paper: "📄"
+            case .scissors: "✂️"
+        }
+    }
+
+    var counterMove: MoveChoice {
+        switch self {
+            case .rock: .paper
+            case .paper: .scissors
+            case .scissors: .rock
+        }
+    }
 }
 
-let moveChoiceToEmoji: Dictionary<MoveChoice, String> = [
-    .rock: "🪨",
-    .paper: "📄",
-    .scissors: "✂️"
-]
-
-let winningMoves: Dictionary<MoveChoice, MoveChoice> = [
-    .rock: .paper,
-    .paper: .scissors,
-    .scissors: .rock
-]
-
-let MAX_NUM_QUESTIONS = 10
+let maxNumQuestions = 10
 
 struct ContentView: View {
-    @State private var opponentChoice = (MoveChoice.allCases.randomElement() ?? .rock)
+    @State private var opponentChoice = MoveChoice.allCases.randomElement()!
     @State private var shouldWin = Bool.random()
     @State private var score = 0
     @State private var questionNum = 1
     @State private var showGameOver = false
     
-    var winningAnswer: MoveChoice {
-        winningMoves[opponentChoice] ?? .rock
-    }
-    
     func ask() {
         opponentChoice = MoveChoice.allCases.randomElement() ?? .rock
         shouldWin.toggle()
+    }
+    
+    func scoreAnswer(_ moveChoice: MoveChoice) -> Int {
+        if moveChoice == opponentChoice {
+            return 0
+        } else if (shouldWin && moveChoice == opponentChoice.counterMove) || (!shouldWin && moveChoice != opponentChoice.counterMove) {
+            return 1
+        } else {
+            return -1
+        }
     }
     
     func reset() {
@@ -62,7 +72,7 @@ struct ContentView: View {
                 
                 Spacer()
                 
-                Text(moveChoiceToEmoji[opponentChoice] ?? "none")
+                Text(opponentChoice.emoji)
                     .font(.system(size: 200))
                 
                 
@@ -74,31 +84,26 @@ struct ContentView: View {
                 
                 HStack {
                     ForEach(MoveChoice.allCases, id: \.self) { moveChoice in
-                        Button() {
-                            if moveChoice == opponentChoice {
-                                // no-op - no score change
-                            } else if (shouldWin && moveChoice == winningAnswer) || (!shouldWin && moveChoice != winningAnswer) {
-                                score += 1
-                            } else {
-                                score -= 1
-                            }
+                        Button {
+                            score += scoreAnswer(moveChoice)
                             
-                            if questionNum == MAX_NUM_QUESTIONS {
+                            if questionNum == maxNumQuestions {
                                 showGameOver = true
                             } else {
                                 questionNum += 1
                                 ask()
                             }
                         } label: {
-                            Text(moveChoiceToEmoji[moveChoice] ?? "none")
+                            Text(moveChoice.emoji)
                                 .font(.system(size: 100))
                         }
-                        .alert("Game Over", isPresented: $showGameOver) {
-                            Button("Try again", action: reset)
-                        } message: {
-                            Text("Final score: \(score)")
-                        }
+                        
                     }
+                }
+                .alert("Game Over", isPresented: $showGameOver) {
+                    Button("Try again", action: reset)
+                } message: {
+                    Text("Final score: \(score)")
                 }
                 
                 Spacer()
